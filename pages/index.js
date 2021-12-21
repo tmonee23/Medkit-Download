fa7ad264dcc567a2173be225cfea3d5842777fbe
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2021-12-21 21:37:00 +0100

    Final Link Change
    Final Commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
@@ -12,7 +11,8 @@ export default function Home() {
 
       <main>
         <p className="description">
-          <a href="https://gateway.pinata.cloud/ipfs/QmPHmWRpxveipMVAZCvHnPsv9KhnsqKqWbemXFSsGA8ahN">Merry Christmas!</a>
+          <a href="https://medkitdaniel.s3.eu-central-1.amazonaws.com/Medkit-86d6c38723a743a6a5ae04e0937a00d6-signed.apk">Merry Christmas!</a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Download the medkit file</a>
         </p>
       </main>
     </div>
