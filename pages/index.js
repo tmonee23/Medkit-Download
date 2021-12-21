431a8ea53176c49b245067649d12d0db72bfa047
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2021-12-21 21:38:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <p className="description">
+        <h1 className="description">
           <a href="https://medkitdaniel.s3.eu-central-1.amazonaws.com/Medkit-86d6c38723a743a6a5ae04e0937a00d6-signed.apk">Merry Christmas!</a>
+        </h1>
+        <p className="link">
           <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Download the medkit file</a>
         </p>
       </main>
